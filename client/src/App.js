467d8633807f849df896385946ba97c6228bfac4
Seth Lachman <lachmanseth17@gmail.com>
2019-06-10 17:10:04 -0400

    Added react router and navigation to app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
 import Register from "./components/Register";
 import Users from "./components/Users";
 
 function App() {
   return (
     <div>
-      <Register />
+      <Navigation />
+      <Route exact path="/" component={Users} />
+      <Route path="/register" component={Register} />
     </div>
   );
 }
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Register</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
