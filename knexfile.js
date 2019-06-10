afe960a070ff76de3cd53966b6b66a610534d991
Seth Lachman <lachmanseth17@gmail.com>
2019-06-10 15:10:43 -0400

    Added database and knexfile.

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/auth.db3"
+    },
+    // SQLite will not enforce foreign keys by default
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  }
+};
